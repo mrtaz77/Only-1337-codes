e2ce883f3815ac77b75ea49ebdd5f3c352d2bcc0
icarus77 <113765142+mrtaz77@users.noreply.github.com>
2025-03-13 15:13:43 +0600

    Time: 44 ms (15.99%), Space: 18 MB (11.36%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -1,18 +1,24 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
+        _a , _b, _c = a, b, c
         flips = 0
-        while a > 0 or b > 0 or c > 0:
-            lsb_a = a & 1
-            lsb_b = b & 1
-            lsb_c = c & 1
-            flips += (((lsb_a | lsb_b) ^ 1) & lsb_c)
+        while _c > 0:
+            lsb_a, lsb_b, lsb_c = _a & 1, _b & 1, _c & 1
+            flips += ((lsb_a ^ 1) & (lsb_b ^ 1) & lsb_c)
             flips += ((lsb_c ^ 1) & lsb_a)
             flips += ((lsb_c ^ 1) & lsb_b)
-            a >>= 1
-            b >>= 1
-            c >>= 1
+            _c = _c >> 1
+            _a = _a >> 1
+            _b = _b >> 1
+        while _a > 0:
+            flips += (_a & 1)
+            flips += (_b & 1)
+            _a = _a >> 1
+            _b = _b >> 1
+        while _b > 0:
+            flips += (_b & 1)
+            _b = _b >> 1
         return flips
-            
         
 
         
