97f907afc6c9cce709e5c933871bbc354c9b15fc
icarus77 <113765142+mrtaz77@users.noreply.github.com>
2025-03-14 07:57:57 +0600

    Time: 4 ms (78.42%), Space: 18.9 MB (61.82%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,28 +1,24 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        i = 0
         stack = []
         for asteroid in asteroids:
-            stack.append(asteroid)
-            if asteroid > 0:
-                break
-            i += 1
-        for j in range(i+1, len(asteroids)):
-            curr = asteroids[j]
-            if curr > 0 or len(stack) == 0:
-                stack.append(curr)
+            last = 0
+            if stack and asteroid < 0 and stack[-1] > 0:
+                last = 0
+                while stack and stack[-1] > 0 and stack[-1] < -asteroid:
+                   last = stack.pop()
+                if stack: 
+                    if stack[-1] == -asteroid:
+                        stack.pop()
+                    elif stack[-1] < 0:
+                        stack.append(asteroid)
+                elif last < -asteroid:
+                    stack.append(asteroid)
             else:
-                top = stack.pop()
-                while top < -curr and top > 0 and len(stack) > 0:
-                    top = stack.pop()
-                if top > -curr:
-                    stack.append(top)
-                elif top < 0:
-                    stack.append(top)
-                    stack.append(curr)
-                elif top < -curr:
-                    stack.append(curr)
+                stack.append(asteroid)
+            
         return stack
+            
                 
 
 
